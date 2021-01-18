76f574eb44700041cda346bd58043de225a04047
Rahul kumar <rahulkumar1401995@gmail.com>
2021-01-18 21:20:28 +0530

    update

--- database/education (1).sql
+++ database/education (1).sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 09, 2021 at 03:46 PM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 5.6.40
+-- Generation Time: Jan 18, 2021 at 04:49 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -31,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `class` (
   `id` int(50) NOT NULL,
   `class` varchar(255) NOT NULL,
-  `added_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `added_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -39,35 +38,36 @@ CREATE TABLE `class` (
 --
 
 INSERT INTO `class` (`id`, `class`, `added_on`) VALUES
-(1, '1', '2021-01-06 17:14:46'),
-(2, '12', '2021-01-06 17:14:58');
+(1, '1', '2021-01-06 11:44:46'),
+(2, '12', '2021-01-06 11:44:58');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `notes`
+-- Table structure for table `students`
 --
 
-CREATE TABLE `notes` (
-  `note_id` int(50) NOT NULL,
-  `class_id` int(50) NOT NULL,
-  `subject_id` int(50) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `notes` longtext NOT NULL,
-  `addedon` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `students` (
+  `student_id` int(11) NOT NULL,
+  `fullname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `phone_` varchar(15) DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `profile_pic` text DEFAULT NULL,
+  `account_type` enum('Basic','Paid') DEFAULT 'Basic',
+  `class_id` int(11) DEFAULT NULL,
+  `updated_at` date DEFAULT NULL,
+  `reg_on` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `notes`
+-- Dumping data for table `students`
 --
 
-INSERT INTO `notes` (`note_id`, `class_id`, `subject_id`, `title`, `notes`, `addedon`) VALUES
-(1, 2, 2, 'zvsv', 'zvsv', '2021-01-09 14:24:51'),
-(2, 2, 2, 'zvsv', 'zvsv', '2021-01-09 14:25:01'),
-(3, 2, 2, 'zvsv', 'zvsv', '2021-01-09 14:25:18'),
-(4, 1, 1, 'c ', 'c ', '2021-01-09 14:28:19'),
-(5, 2, 1, 'Quality', '', '2021-01-09 14:28:59'),
-(6, 2, 3, 'zdb', '<h2>What is Lorem Ipsum?</h2>\r\n\r\n<p><strong>Lorem Ipsum</strong>&nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n\r\n<h2>Why do we use it?</h2>\r\n\r\n<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &#39;Content here, content here&#39;, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for &#39;lorem ipsum&#39; will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>\r\n', '2021-01-09 14:31:03');
+INSERT INTO `students` (`student_id`, `fullname`, `email`, `phone_`, `password`, `profile_pic`, `account_type`, `class_id`, `updated_at`, `reg_on`) VALUES
+(1, 'rahul kumar', 'admin@admin.com', '09953470167', '3921aecaf4d316747fbf7e563b4bd36b8488e739e639e523b106815f225b610f9bacf0653235bdeceecfe7c219915e38633109a515c5430105e30fa81f10693cjnaCTSDiwO89Qvo4IhwLeLT5H1yPSXC5Vf8SdF13gvU=', NULL, 'Basic', NULL, NULL, '2021-01-06 22:50:38'),
+(2, 'Test', 'admin@admin.com', '09953470167', '1cd4972fafc902fd504783c48a107ab067e3448f3ca8857ae58e64b16bc2030892f45c990851af07fe2cf2f205dbac43ec02c7575ee92e31c45cdb803fa612feklRglvdeDnbGdQX60hCJFx9bFd014Ctcs2Mh/VNpDAw=', NULL, 'Basic', NULL, NULL, '2021-01-06 22:52:22'),
+(3, 'fasfsdf', 'rohansingh14061995@gmail.com', '3333', '280f53205f2ea43a3b96c7cb2a3058746e67786a9febe19c229abbabb27aea108f7af3f5e2ac2cd81a003a5192b5fa7a280c61d922abd2d29b0bca00f11c1612R5ejTcGTiGjdJC+Be7gxEnyDG8iyfUP4kgR84ALsAvI=', NULL, 'Basic', NULL, NULL, '2021-01-06 22:53:21');
 
 -- --------------------------------------------------------
 
@@ -78,7 +78,7 @@ INSERT INTO `notes` (`note_id`, `class_id`, `subject_id`, `title`, `notes`, `add
 CREATE TABLE `subject` (
   `id` int(50) NOT NULL,
   `subject` varchar(255) NOT NULL,
-  `added_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `added_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -86,22 +86,25 @@ CREATE TABLE `subject` (
 --
 
 INSERT INTO `subject` (`id`, `subject`, `added_on`) VALUES
-(1, 'Math', '2021-01-06 17:08:41'),
-(2, 'English', '2021-01-06 17:09:57'),
-(3, 'Hindi', '2021-01-06 17:10:32');
+(1, 'Math', '2021-01-06 11:38:41'),
+(2, 'English', '2021-01-06 11:39:57'),
+(3, 'Hindi', '2021-01-06 11:40:32'),
+(5, 'test', '2021-01-09 15:49:27');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user_contacts`
+-- Table structure for table `subject_notes`
 --
 
-CREATE TABLE `user_contacts` (
-  `id` int(50) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `message` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE `subject_notes` (
+  `note_id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `description_` text NOT NULL,
+  `added_on` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -115,7 +118,7 @@ CREATE TABLE `video` (
   `subject_id` int(50) NOT NULL,
   `title` varchar(255) NOT NULL,
   `video_url` varchar(255) NOT NULL,
-  `added_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `added_on` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
@@ -123,7 +126,9 @@ CREATE TABLE `video` (
 --
 
 INSERT INTO `video` (`id`, `class_id`, `subject_id`, `title`, `video_url`, `added_on`) VALUES
-(1, 1, 2, 'drheh', 'video-2021-01-07-12-18-49asdas.mp4', '2021-01-07 11:18:49');
+(1, 1, 2, 'drheh', 'video-2021-01-07-12-18-49asdas.mp4', '2021-01-07 05:48:49'),
+(2, 1, 2, 'test Teee', 'video-2021-01-07-12-18-49asdas.mp4', '2021-01-09 13:55:42'),
+(3, 1, 2, 'Url Ofr Test', 'video-2021-01-07-12-18-49asdas.mp4', '2021-01-09 13:55:42');
 
 --
 -- Indexes for dumped tables
@@ -136,21 +141,15 @@ ALTER TABLE `class`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `notes`
+-- Indexes for table `students`
 --
-ALTER TABLE `notes`
-  ADD PRIMARY KEY (`note_id`);
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
 
 --
 -- Indexes for table `subject`
 --
 ALTER TABLE `subject`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_contacts`
---
-ALTER TABLE `user_contacts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -170,28 +169,22 @@ ALTER TABLE `class`
   MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `notes`
+-- AUTO_INCREMENT for table `students`
 --
-ALTER TABLE `notes`
-  MODIFY `note_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `user_contacts`
---
-ALTER TABLE `user_contacts`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `video`
 --
 ALTER TABLE `video`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
